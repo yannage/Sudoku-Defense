9b61c53032d74eecc9955b846f64156c0c90feef
Yannage <yannissl4@gmail.com>
2025-04-01 08:02:17 -0400

    update init.js

--- js/init.js
+++ js/init.js
@@ -2,8 +2,6 @@
  * init.js - Script to load and initialize game modules in the correct order
  * This file should be included last in the HTML
  */
- 
- //yes
 
 // Wait for all modules to be loaded
 document.addEventListener('DOMContentLoaded', function() {
